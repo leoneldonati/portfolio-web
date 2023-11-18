0f5fe387483dd3a16776e9a3c1444d2217bc3c86
Leonel <leonelroman321@gmail.com>
2023-11-17 22:46:29 -0300

    changes on architecture

--- src/services/github.ts
+++ src/services/github.ts
@@ -1,45 +1,49 @@
 import type { GitHubProject } from "../interfaces";
 
 const token = import.meta.env.PUBLIC_GITHUB;
-const GITHUB_URL = 'https://api.github.com/users/leoneldonati/repos?per_page=11'
+const GITHUB_URL =
+  "https://api.github.com/users/leoneldonati/repos?per_page=12";
 const CONFIG = {
   headers: {
     Authorization: `token ${token}`,
   },
-}
+};
 let repos: GitHubProject[] | null = null;
 
 // obtener todos los repositorios
-export async function getRepos (): Promise<GitHubProject[] | undefined> {
+export async function getRepos(): Promise<GitHubProject[] | undefined> {
   try {
-    const res = await fetch(
-      GITHUB_URL,
-      CONFIG
-    );
+    const res = await fetch(GITHUB_URL, CONFIG);
     const data = await res.json();
 
-    const reposFiltered = data.map(repos => ({
-      id: repos.id,
-      name: repos.name,
-      htmlUrl: repos.html_url,
-      description: repos.description,
-      createdAt: repos.created_at,
-      pushedAt: repos.pushed_at,
-      homepage:  repos.home_page,
-      stargazersCount: repos.stargazers_count,
-      language: repos.language,
-      forksCount: repos.forks_count
-    }))
-    
-    repos = reposFiltered
-    
-    return reposFiltered
+    const reposFiltered = data
+      .map((repos) => ({
+        id: repos.id,
+        name: repos.name,
+        htmlUrl: repos.html_url,
+        description: repos.description,
+        createdAt: repos.created_at,
+        pushedAt: repos.pushed_at,
+        homepage: repos.home_page,
+        stargazersCount: repos.stargazers_count,
+        language: repos.language,
+        forksCount: repos.forks_count,
+      }))
+      .filter((repo) => {
+        const blackList = ["portfolio-web", "reset-css", "leoneldonati"];
+
+        return !blackList.includes(repo.name.toLowerCase());
+      });
+
+    repos = reposFiltered;
+
+    return reposFiltered;
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
 
 // obtener un solo repositorio
-export function getOneRepo (id: number): GitHubProject | undefined {
-  return repos?.filter(repo => repo.id === id)[0]
+export function getOneRepo(id: number): GitHubProject | undefined {
+  return repos?.filter((repo) => repo.id === id)[0];
 }
