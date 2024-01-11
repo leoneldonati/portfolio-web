c60a6eac994b0079c10b1cdc9be8f1195d8312d3
Leonel <leonelroman321@gmail.com>
2024-01-10 22:27:40 -0300

    complete rework

--- src/store.tsx
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand'
-import type { ProjectStore } from './interfaces'
-
-export const useProjectsStore = create<ProjectStore>((set) => ({
-  selectedProject: {},
-  setSelectedProject: (selectedProject) => set({ selectedProject })
-}))
