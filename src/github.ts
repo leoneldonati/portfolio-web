de1466289e8de0ea9e3f35732adeecff0ed341a7
Leonel <leonelroman321@gmail.com>
2024-01-16 13:14:05 -0300

    converted promises function in async/await, filtered unnecessary repos

--- src/github.ts
+++ src/github.ts
@@ -6,7 +6,6 @@ const CONFIG_FETCH = {
   },
 };
 
-
 export interface RepoInfo {
   name: string;
   description: string;
@@ -19,24 +18,29 @@ export interface RepoInfo {
   pushedAt: Date;
 }
 
-export function getRepos(): Promise<Array<RepoInfo> | void> {
-  return fetch(URL, CONFIG_FETCH)
-    .then((res) => res.json())
-    .then((repos) => {
-      return repos.map((repos: any) => ({
-        name: repos.name,
-        description: repos.description,
-        projectUrl: repos.html_url,
-        lang: repos.language,
-        homepage: repos.homepage,
-        visibility: repos.private,
-        createdAt: repos.created_at,
-        updatedAt: repos.updated_at,
-        pushedAt: repos.pushed_at,
-      }))
-    })
-    .catch((error) => {
-      console.log(error);
-      throw new Error(error);
-    });
+const BLACK_LIST = ['reset-css', 'leoneldonati', 'portfolio-web']
+
+export async function getRepos(): Promise<Array<RepoInfo> | void> {
+  try {
+    const res = await fetch(URL, CONFIG_FETCH);
+
+    const repos = await res.json();
+
+    const projects = repos.map((repos: any) => ({
+      name: repos.name,
+      description: repos.description,
+      projectUrl: repos.html_url,
+      lang: repos.language,
+      homepage: repos.homepage,
+      visibility: repos.private,
+      createdAt: repos.created_at,
+      updatedAt: repos.updated_at,
+      pushedAt: repos.pushed_at,
+    })).filter((repos: RepoInfo) => !BLACK_LIST.includes(repos.name.toLowerCase()));
+
+    return projects;
+  } catch (error) {
+    console.log(error);
+    throw new Error(String(error));
+  }
 }
