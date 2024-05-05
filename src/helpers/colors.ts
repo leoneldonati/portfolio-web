c90f08bb9bb070629e437cb44705307f6dd94221
Leonel Donati <leonelroman321@gmail.com>
2024-05-05 15:46:40 -0300

    changed project structure

--- src/helpers/colors.ts
+++ src/helpers/colors.ts
@@ -1,12 +1,12 @@
 export function setColorByLang (lang: string): string  {
   switch (lang) {
-    case 'TypeScript':
+    case 'ts':
       return '#1E90FF';
-    case 'JavaScript':
+    case 'js':
       return '#FFD700';
     case 'Astro':
       return '#DC143C';
-    case 'HTML':
+    case 'html':
       return '#FF0000';
     default:
       return '#c8c8c8';
