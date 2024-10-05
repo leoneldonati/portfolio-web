7627824c14403aa65ef091e11b71be7c663ca188
Leonel Donati <leonelroman321@gmail.com>
2024-10-05 10:33:11 -0300

    about and project section v.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  adapter: netlify()
 });
---
+++ src/scripts/colors.ts
@@ -0,0 +1,5 @@
+export function setColorByLang (lang: string) {
+  if (lang === 'Astro') return '#DC143C'
+  if (lang === 'TypeScript') return '#00008B'
+  if (lang === 'HTML') return '#8B0000'
+}
