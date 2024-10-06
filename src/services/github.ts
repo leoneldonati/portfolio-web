49fd25659be2ed4da8f8f60c7d085a0e4c0e0e09
Leonel Donati <leonelroman321@gmail.com>
2024-10-06 00:54:22 -0300

    fixed some styles

--- src/services/github.ts
+++ src/services/github.ts
@@ -1,20 +1,26 @@
 export async function getRepos(n?: number) {
-  const res = await fetch(`https://api.github.com/users/leoneldonati/repos?q=${n ?? 10}`, {
-    headers: {
-      'Authorization': `Bearer ${import.meta.env.GITHUB_TOKEN}`
+  const res = await fetch(
+    `https://api.github.com/users/leoneldonati/repos?q=${n ?? 10}`,
+    {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.GITHUB_TOKEN}`,
+      },
     }
-  })
+  );
 
-  if (!res.ok) return {
-    ok: res.ok,
-    error: await res.json()
-  }
+  if (!res.ok)
+    return {
+      ok: res.ok,
+      error: await res.json(),
+    };
 
-  const WHITE_LIST = ['chatly', 'books-app', 'portfolio-web', 'ask']
-  const filteredRepos = [...await res.json()].filter(repo => WHITE_LIST.includes(repo.name))
+  const WHITE_LIST = ["chatly", "books-app", "portfolio-web", "ask"];
+  const filteredRepos = [...(await res.json())].filter((repo) =>
+    WHITE_LIST.includes(repo.name)
+  );
 
   return {
     ok: res.ok,
-    data: filteredRepos
-  }
+    data: filteredRepos,
+  };
 }
